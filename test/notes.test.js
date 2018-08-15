66be2d27585f485f199ec952787749fe5e275465
Tarik Desire <tprdesire@gmail.com>
2018-08-15 15:28:07 -0400

    Working on tests

---
+++ test/notes.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const app = require('../server');
+const { TEST_MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+const seedNotes = require('../db/seed/notes');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe ('Node Noteful Tests', function() {
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function () {
+    return Note.insertMany(seedNotes);
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+  describe('GET /api/notes', function() {
+    it('should return the default array of notes', function() {
+      return Promise.all([
+        Note.find(),
+        chai.request(app).get('/api/notes')
+      ])
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+          expect(res.body[0]).to.be.a('object');
+          expect(res.body[0]).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+        });
+    });
+
+    /*it('should return correct search results for a valid query', function() {
+      let data;
+
+      return Note.findOne()
+        .then(_data => {
+          data = _data;
+          return chai.request(app).get(`api/notes?searchTerm=${data.title}`);
+        })
+        .then((res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.
+        })
+    })*/
+  });
+});
+
+
+
