7d8a525b71bf940e53bd0abecc4171858665491d
Tarik Desire <tprdesire@gmail.com>
2018-08-16 15:28:37 -0400

    Added folders schema, updated notes schema, re-seeded db with new models

---
+++ models/folder.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true}
+});
+
+folderSchema.set('timestamps', true);
+
+folderSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
+
+module.exports = mongoose.model('Folder', folderSchema);
+
--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,14 +4,19 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
 const seedNotes = require('../db/seed/notes');
+const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
-  .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes()
+    ]);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
