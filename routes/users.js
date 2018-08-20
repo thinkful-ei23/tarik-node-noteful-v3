69824827faba6dc5a1334bd543e01479112774e1
Tarik Desire <tprdesire@gmail.com>
2018-08-20 12:41:11 -0400

    Added userSchema, mounted users router, and added POST endpoint for creating a new user

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { username, password, fullname } = req.body;
+
+  return User.create({
+    username,
+    password,
+    fullname
+  })
+    .then(user => {
+      return res.status(201).location(`/api/users/${user.id}`).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+});
+
+module.exports = router;
