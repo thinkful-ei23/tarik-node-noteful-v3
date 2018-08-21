745e2f53938b4ed45f423057c6308da6d5f94ae9
Tarik Desire <tprdesire@gmail.com>
2018-08-21 14:50:30 -0400

    Added jtw, .env file, and refactored POST login endpoint to return jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const { JWT_SECRET, JWT_EXPIRY } = config;
 
 const router = express.Router();
 
@@ -9,8 +13,16 @@ const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY,
+  });
+}
+
 router.post('/', localAuth, (req, res) => {
-  return res.json(req.user); 
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const User = require('../models/user');
-
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
